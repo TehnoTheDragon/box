61b869d9949da4b58bb9f1a3cd4f8b6eda144cae
TehnoTheDragon <tehnothedragon@gmail.com>
2023-04-30 18:50:15 +0300

    ParserRule

--- box/src/box/frontend/parser/parser_rule.cpp
+++ box/src/box/frontend/parser/parser_rule.cpp
@@ -1,5 +1,22 @@
-#include "box/frontend/parser/ParserRule.hpp"
+#include "box/frontend/parser/parser_rule.hpp"
 
 namespace box::frontend {
-	
+	// PaerserRule :: Value
+
+	bool ParserRule::Value::operator==(const std::string& value) {
+		return std::count(this->strs.begin(), this->strs.end(), value);
+	}
+
+	bool ParserRule::Value::operator==(const TokenType& value) {
+		return std::count(this->ids.begin(), this->ids.end(), value);
+	}
+
+	// Parser Rule
+
+	ParserRule::ParserRule(const uint32_t& id) : _ID(id) {
+
+	}
+	ParserRule::~ParserRule()
+	{
+	}
 }
