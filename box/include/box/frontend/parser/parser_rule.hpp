df74695ec54b8ae2abaa7d17f70815052f8a7a44
TehnoTheDragon <tehnothedragon@gmail.com>
2023-04-30 18:49:59 +0300

    ParserRule

--- box/include/box/frontend/parser/parser_rule.hpp
+++ box/include/box/frontend/parser/parser_rule.hpp
@@ -1,8 +1,43 @@
 #pragma once
 #include <string>
 #include <vector>
-#include "../lexer/Token.hpp"
+#include "../lexer/token.hpp"
 
 namespace box::frontend {
-	
+	class ParserRule {
+		const uint32_t _ID;
+	public:
+        enum class ValueType {
+            VALUE, ID
+        };
+
+        struct Value {
+            ParserRule* parent;
+            ValueType type;
+            std::vector<std::string> strs;
+            std::vector<TokenType> ids;
+
+            Value(const std::string value) : type(ValueType::VALUE) {
+                this->strs = { value };
+            }
+
+            Value(std::vector<std::string>&& value) : type(ValueType::VALUE) {
+                this->strs = std::move(value);
+            }
+
+            Value(const TokenType value) : type(ValueType::ID) {
+                this->ids = { value };
+            }
+
+            Value(std::vector<TokenType>&& value) : type(ValueType::VALUE) {
+                this->ids = std::move(value);
+            }
+
+            bool operator == (const std::string& value);
+            bool operator == (const TokenType& value);
+        };
+	public:
+        ParserRule(const uint32_t& id);
+        ~ParserRule();
+	};
 }
