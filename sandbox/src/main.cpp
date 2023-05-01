e9543ba29de475400cfe88c013533617dff22ecd
TehnoTheDragon <tehnothedragon@gmail.com>
2023-05-01 12:51:55 +0300

    still, just testing in sandbox

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -3,15 +3,48 @@
 #include <box/frontend/parser/parser.hpp>
 #include <box/frontend/parser/parser_rule.hpp>
 
+#include <asmjit/asmjit.h>
+
+using namespace asmjit;
 using namespace box::frontend;
 
-int main(int argc, char** argv) {
-	/*ParserRule varDefRule = ParserRule(0);
-	
-	auto parser = Parser(lexers::getBoxLexer(), {
-		varDefRule,
-	});*/
+typedef int (*Func)(int, int);
 
-	std::system("pause");
-	return 0;
+int main(int argc, char** argv) {
+    using namespace x86;
+
+    JitRuntime jit;
+    CodeHolder code;
+    code.init(jit.environment(), jit.cpuFeatures());
+    Compiler cc(&code);
+
+    FuncNode* entry = cc.addFunc(FuncSignatureT<int, int, int>());
+
+    Gp vX = cc.newGpd();
+    Gp vY = cc.newGpd();
+
+    entry->setArg(0, vX);
+    entry->setArg(1, vY);
+
+    cc.add(vX, vY);
+    cc.ret(vX);
+
+    cc.endFunc();
+    cc.finalize();
+
+    Func fn;
+    Error err = jit.add(&fn, &code);
+    if (err) {
+        printf("AsmJit: %s\n", DebugUtils::errorAsString(err));
+        std::system("pause");
+        return 1;
+    }
+
+    int result = fn(5, 25);
+    printf("%d\n", result);
+
+    jit.release(fn);
+
+    std::system("pause");
+    return 0;
 }
